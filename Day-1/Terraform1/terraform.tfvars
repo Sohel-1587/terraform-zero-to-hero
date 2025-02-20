1118e24ffea87a9b98c0b0c47895237ae649ec5d
Sohel-1587 <143531120+Sohel-1587@users.noreply.github.com>
2025-02-20 13:41:50 +0000

    test-terraform

---
+++ Day-1/Terraform1/terraform.tfvars
@@ -0,0 +1,2 @@
+aws_value = "ami-0c7af5fe939f2677f"
+aws_instance_type = "t2.micro"
---
+++ Day-1/Terraform1/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_value" {
+  description = "value for aws ec2 ami"
+}
+
+variable "aws_instance_type" {
+  description = "value for aws instance type"
+}
